f7f7c7f3ff40065bf3d0fcb77585763d46f5f083
Vikas kumar gupt <vikaskgupt9044@gmail.com>
2026-08-09 08:12:23 +0530

    pattern printting

---
+++ 06-pattern_printting.py
@@ -0,0 +1,10 @@
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print("*", end=" ")
+#     print()
+    
+    
+for row in range(1,6):
+    for col in range(1,6):
+        print(col, end=" ")
+    print()
