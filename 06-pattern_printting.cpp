2158f7d735ef9d82f87128d812b363591b95d936
Vikas kumar gupt <vikaskgupt9044@gmail.com>
2026-08-10 08:25:26 +0530

    pattern printing Day 2

--- 06-pattern_printting.cpp
+++ 06-pattern_printting.cpp
@@ -9,10 +9,45 @@ int main(){
     //     cout<<endl;
     // }
 
-    for(row=1; row<=5;row++){
-        for(col=1;col<=5;col++){
-            cout<<row<<" ";
+    // for(row=1; row<=5;row++){
+    //     for(col=1;col<=5;col++){
+    //         cout<<row<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+    // 1 4 9 16 25 
+    // 1 4 9 16 25 
+    // 1 4 9 16 25 
+    // 1 4 9 16 25 
+    // 1 4 9 16 25
+    // for(row=1;row<=5;row=row+1){
+    //     for(col=1; col<=5;col++){
+    //         cout<<col*col<<" ";
+    //     }
+    //     cout<<endl;
+
+    // }
+
+
+
+    // a a a a a 
+    // b b b b b 
+    // c c c c c 
+    // d d d d d 
+    // e e e e e 
+    for(row=1;row<=5;row++){
+        for(col=1; col<=5; col++){
+            char name='a'+(row-1);
+            cout<<name<<" ";
         }
         cout<<endl;
+
     }
 }
+
+
+
--- 06-pattern_printting.py
+++ 06-pattern_printting.py
@@ -4,7 +4,30 @@
 #     print()
     
     
+# for row in range(1,6):
+#     for col in range(1,6):
+#         print(col, end=" ")
+#     print()
+
+
+# 1 4 9 16 25 
+# 1 4 9 16 25 
+# 1 4 9 16 25 
+# 1 4 9 16 25 
+# 1 4 9 16 25
+# for row in range(1,6):
+#     for col in range(1,6):
+#         print(col*col,end=" ")
+#     print()
+
+
+# a a a a a 
+# b b b b b 
+# c c c c c 
+# d d d d d 
+# e e e e e 
 for row in range(1,6):
     for col in range(1,6):
-        print(col, end=" ")
-    print()
+        name=chr(ord("a")+(row-1))
+        print(name,end=" ")
+    print();
